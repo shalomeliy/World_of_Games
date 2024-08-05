faac96d685c6e43bab06dc1cbdaf2282e6e6b3c0
Shalom Eliyahu <shalom.eliy@gmail.com>
2024-08-05 22:41:24 +0300

    Update e2e.py

--- e2e.py
+++ e2e.py
@@ -10,18 +10,25 @@ from selenium.webdriver.common.by import By
 
 def test_scores_service():
     chrome_options = ChromiumOptions()
+    chrome_driver_path = ChromeDriverManager().install()
+    print(f"ChromeDriver Path: {chrome_driver_path}")
 
-    service = Service(ChromeDriverManager().install(), options=chrome_options)
-    driver_chrome = webdriver.Chrome(service=service)
+    service = Service(chrome_driver_path, log_path='chromedriver.log')
+    try:
+        driver_chrome = webdriver.Chrome(service=service, options=chrome_options)
 
-    driver_chrome.get("http://127.0.0.1:8777/")
-    score = driver_chrome.find_element(By.ID, "score")
-    if 0 <= int(score.text) <= 1000:
-        driver_chrome.quit()
-        return True
-    else:
-        driver_chrome.quit()
+        driver_chrome.get("http://127.0.0.1:8777/")
+        score = driver_chrome.find_element(By.ID, "score")
+        if 0 <= int(score.text) <= 1000:
+            driver_chrome.quit()
+            return True
+        else:
+            driver_chrome.quit()
+            return False
+    except Exception as e:
+        print(f"Error initializing ChromeDriver: {e}")
         return False
+
 
 
 def main_function():
