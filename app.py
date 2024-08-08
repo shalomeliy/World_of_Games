96804ef432d29043b95f74de28c41ee29712a331
Shalom Eliyahu <shalom.eliy@gmail.com>
2024-08-09 00:53:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ from score import add_score
 score = 0
 
 # Game names and descriptions
-games = ["1. Memory Game - a sequence of numbers will appear for 1 second and you have to guess it back",
-"2. Guess Game - guess a number and see if you chose like the computer",
-"3. Currency Roulette - try and guess the value of a random amount of USD in ILS"]
-
+games = [
+    "1. Memory Game - a sequence of numbers will appear for 1 second and you have to guess it back",
+    "2. Guess Game - guess a number and see if you chose like the computer",
+    "3. Currency Roulette - try and guess the value of a random amount of USD in ILS"
+]
 print("\nWelcome to the World of Game!\n")
 
 
