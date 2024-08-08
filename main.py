5cba9b40c6ac2f1f99c62144b0419f27e784b355
Shalom Eliyahu <shalom.eliy@gmail.com>
2024-08-09 01:48:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+# Importing necessary functions from the 'app' module
 from app import start_play, welcome
 
+# Collect the user's first name using the welcome function
 user_name = welcome()
+
+# Start a selected game with choosing difficulty while pass the user name to the start_play function
 start_play(user_name)
