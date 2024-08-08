5f3eda3e346c7b08cc86cd5034c8389c66cbca55
Shalom Eliyahu <shalom.eliy@gmail.com>
2024-08-09 02:16:04 +0300

    Update score.py
    Add  validation

--- score.py
+++ score.py
@@ -2,28 +2,33 @@ import os
 import utils
 
 
-# This function add points to the DB - Scores.py file, for every user winning
+# This function adds points to the DB - Scores.txt file for every user win
 def add_score(difficulty):
-    # calculate points of winning
-    points_of_winning = (int(difficulty) * 3) + 5
 
-    # check that the file is existed
+    # Calculate points of winning
+    points_of_winning = (difficulty * 3) + 5
+
+    # Check if the scores file exists
     if os.path.exists(utils.scores_file_name):
         # Open the file and read the current score
-        score_file = open("Scores.txt", 'r')
-        current_score = score_file.readline()
+        with open(utils.scores_file_name, 'r') as score_file:
+            current_score = score_file.readline().strip()
+
+        # Validate if current_score is a digit
         if current_score.isdigit():
             current_score = int(current_score)
         else:
             current_score = 0
+    else:
+        # If the file does not exist, initialize the score to 0
+        current_score = 0
 
-        # add the new score to exist score
-        new_score = int(current_score) + points_of_winning
+    # Calculate new score
+    new_score = current_score + points_of_winning
 
-        # Write the new score to the file
-        score_file = open("Scores.txt", 'w+')
+    # Write the new score to the file
+    with open(utils.scores_file_name, 'w') as score_file:
         score_file.write(str(new_score))
-        score_file.close()
 
-# example usage
-# add_score(2)
+# Example usage:
+#add_score(3)
